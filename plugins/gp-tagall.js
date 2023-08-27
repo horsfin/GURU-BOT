f146e2e5835a035a929ccf2737410af2d2493342
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 11:38:13 +0300

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ GURU ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`▢ Группа : *${groupMetadata.subject}*\n▢ Участник : *${participants.length}*${text ? `\n▢ Сообщение : ${text}\n` : ''}\n┌───⊷ *Упоминание*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ RIP ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
     })
 }
