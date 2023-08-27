7f288938f672e61ccd889989537184d62f2f8312
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 11:42:34 +0300

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('✅ The welcome message is configured')
-  } else throw `✳️ Enter the Welcome message\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
+    m.reply('✅ Приветствие поставлено')
+  } else throw `✳️ Введите сообщение приветствия\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
