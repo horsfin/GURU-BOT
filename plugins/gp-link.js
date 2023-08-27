acbbf869479a9ada89cf5699269277d06736a779
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 11:33:57 +0300

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -2,13 +2,13 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Can only be used in groups'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Можно использовать только в группах'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
+    if (!('participants' in groupMetadata)) throw 'участники не определены :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw '✳️ I am not in that group :('
-    if (!me.admin) throw '✳️ I am not an administrator'
+    if (!me) throw '✳️ Я не пренадлежу данной группе :('
+    if (!me.admin) throw '✳️ Я не админ'
     m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['link']
