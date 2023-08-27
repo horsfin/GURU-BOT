be3779d10643dff752672ff90f089509a1ffb03e
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 10:40:55 +0300

    Update econ-adventure.js

--- plugins/econ-adventure.js
+++ plugins/econ-adventure.js
@@ -3,21 +3,21 @@ let handler = async (m, { usedPrefix }) => {
     let user = global.db.data.users[m.sender]
     let timers = (cooldown - (new Date - user.lastadventure))
     if (user.health < 80) return m.reply(`
-Requires at least 80 ❤️Healths for the adventure!!
-please buy ❤️Healths first by typing *${usedPrefix}buy potion <quantity>*,
-and type *${usedPrefix}heal <quantity>* to use potions
+Для этого приключения требуется 80 исцлений
+пожалуйста сначала купите ❤️Healths набрав*${usedPrefix}buy potion <quantity>*,
+и введите *${usedPrefix}heal <quantity>* что бы использовать зелье
 `.trim())
     if (new Date - user.lastadventure <= cooldown) return m.reply(`
-You're already adventure!!, please wait *🕐${timers.toTimeString()}*
+Ты готов к преключению!!, подожди *🕐${timers.toTimeString()}*
 `.trim())
     const rewards = reward(user)
-    let text = 'you\'ve been adventure and lost'
+    let text = 'Ты побывал в преключении и заблудился'
     for (const lost in rewards.lost) if (user[lost]) {
         const total = rewards.lost[lost].getRandom()
         user[lost] -= total * 1
         if (total) text += `\n*${global.rpg.emoticon(lost)}${lost}:* ${total}`
     }
-    text += '\n\nBut you got'
+    text += '\n\nНо у тебя есть'
     for (const rewardItem in rewards.reward) if (rewardItem in user) {
         const total = rewards.reward[rewardItem].getRandom()
         user[rewardItem] += total * 1
@@ -38,39 +38,39 @@ export default handler
 function reward(user = {}) {
     let rewards = {
         reward: {
-            money: 201,
+            деньги: 201,
             exp: 301,
-            trash: 101,
-            potion: 2,
-            rock: 2,
-            wood: 2,
-            string: 2,
-            common: 2 * (user.dog && (user.dog > 2 ? 2 : user.dog) * 1.2 || 1),
-            uncommon: [0, 0, 0, 1, 0].concat(
+            мусор: 101,
+            зелье: 2,
+            камень: 2,
+            дерево: 2,
+            веревка: 2,
+            общем: 2 * (user.dog && (user.dog > 2 ? 2 : user.dog) * 1.2 || 1),
+           редко: [0, 0, 0, 1, 0].concat(
                 new Array(5 - (
                     (user.dog > 2 && user.dog < 6 && user.dog) || (user.dog > 5 && 5) || 2
                 )).fill(0)
             ),
-            mythic: [0, 0, 0, 0, 0, 1, 0, 0, 0].concat(
+            мифический: [0, 0, 0, 0, 0, 1, 0, 0, 0].concat(
                 new Array(8 - (
                     (user.dog > 5 && user.dog < 8 && user.dog) || (user.dog > 7 && 8) || 3
                 )).fill(0)
             ),
-            legendary: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0].concat(
+            легендарный: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0].concat(
                 new Array(10 - (
                     (user.dog > 8 && user.dog) || 4
                 )).fill(0)
             ),
-            iron: [0, 0, 0, 1, 0, 0],
-            gold: [0, 0, 0, 0, 0, 1, 0],
-            diamond: [0, 0, 0, 0, 0, 0, 1, 0].concat(
+            железо: [0, 0, 0, 1, 0, 0],
+            золото: [0, 0, 0, 0, 0, 1, 0],
+            алмазы: [0, 0, 0, 0, 0, 0, 1, 0].concat(
                 new Array(5 - (
                     (user.fox < 6 && user.fox) || (user.fox > 5 && 5) || 0
                 )).fill(0)
             ),
         },
         lost: {
-            health: 101 - user.cat * 4
+            жизнь: 101 - user.cat * 4
         }
     }
     return rewards
