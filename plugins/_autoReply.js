3f3ace568b49884059a887127c76042284d2e1ac
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 19:38:57 +0300

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -1,8 +1,8 @@
 export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
-  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
+  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('открой эту ссылку')) && !m.isBaileys && !m.isGroup) {
+   this.sendMessage(m.chat,{text:`Привет @${m.sender.split('@')[0]}\nдружок\n\n_Я рип бот,для связи с владельцем_\n*напиши* \`\`\`.owner\`\`\` *чтобы получить контакт владельца*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
     you can rent the bot to join a group or contact owner 
