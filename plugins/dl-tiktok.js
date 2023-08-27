61d9a980c29374596c2b5f9fd14f6fea17c4b2d0
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 10:27:51 +0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,25 +1,25 @@
 import { tiktokdl } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!args[0]) throw `✳️ Enter a TikTok link\n\n📌 Example: ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`;
-  if (!args[0].match(/tiktok/gi)) throw `❎ Verify that the link is from TikTok`;
+  if (!args[0]) throw `✳️ Вставьте ссылку на видео\n\n📌 Пример: ${usedPrefix + command} https://vm.tiktok.com/ZMYG2bUh/`;
+  if (!args[0].match(/tiktok/gi)) throw `❎ Убедиетсь что ссылка связана с тик-ток`;
 
   try {
     const { author: { nickname }, video, description } = await tiktokdl(args[0]);
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw;
-    if (!url) throw '❎ Error downloading the video';
+    if (!url) throw '❎ Ошибка загрузки';
 
    
 
     conn.sendFile(m.chat, url, 'tiktok.mp4', `
 ┌─⊷ TIKTOK
-▢ *Nickname:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
+▢ *Никнеим:* ${nickname} ${description ? `\n▢ *Описание:* ${description}` : ''}
 └───────────`, m);
   } catch (error) {
     // Log the error
-    console.error('Error downloading TikTok video:', args[0], error);
+    console.error('Ошибка:', args[0], error);
 
-    m.reply(`❎ Error downloading the video`);
+    m.reply(`❎ Ошибка`);
   }
 };
 
