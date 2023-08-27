a01b67e35d4e2f902570a75692ca9b202f9c2e81
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 11:41:24 +0300

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
-    m.reply('✅ The farewell message was established')
-  } else throw `✳️ enter the message\n@user (mención)`
+    m.reply('✅ Прощание поставлено')
+  } else throw `✳️ введи сообщение\n@user (mención)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
