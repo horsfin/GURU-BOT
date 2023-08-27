3cfd0806f0d3325efb03c55674842c1221268814
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 12:24:15 +0300

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '*[🔎] give url*', m)
+if (!args[0]) return conn.reply(m.chat, '*[🔎] отправь урл*', m)
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
