78287308a6a9bedde55f0d23e0524569f65ee7d9
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-27 12:12:20 +0300

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -5,9 +5,9 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-*≡ SCRIPT*
+*≡ СКРИПТ*
 
-▢ Git : ${_package.homepage}
+▢ Git : СКРЫТО
 `.trim())
     
 }
