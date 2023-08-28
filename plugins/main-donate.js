58513a5afd4f2ef76160b1d1fd83777c19dd345a
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-28 20:19:24 +0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -6,17 +6,11 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     console.log(`Command received: ${command}`);
 
     let message = `
-*We appreciate your support! 🙏*
+*Привет, я ценю вашу поддержку! 🙏*
     
-To help us keep improving, consider following us on Instagram. Every new follower brings us joy and motivation! 
+// Если хочешь закинуть деньжат владельцу и купить xp или кристалы, то пиши команду /owner для связи с владельцем!`;
 
-Our Instagram: @asli_guru_2k23
-
-Remember, even the smallest support can make a huge difference!
-
-Additionally, if you'd like to contribute directly, you can use the attached Google Pay QR code. Just scan it with your Google Pay app to send a donation. Your support means the world to us!`;
-
-    let img = 'https://telegra.ph/file/005c11a341e1f038786e5.jpg'; 
+    let img = 'https://i.postimg.cc/qBNB6LwK/barbaric-horse-killing-France.jpg'; 
 
     console.log("Sending message and image...");
     await conn.sendFile(m.chat, img, 'donation.jpg', message, m, false, rpyp);
