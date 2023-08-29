ececf4df23a52e90297d38bb386d10e531043bcc
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-29 20:05:31 +0300

    Update econ-transfer.js

--- plugins/econ-transfer.js
+++ plugins/econ-transfer.js
@@ -2,33 +2,33 @@ const items = ['diamond', 'exp']
 let confirmation = {}
 
 async function handler(m, { conn, args, usedPrefix, command }) {
-    if (confirmation[m.sender]) return m.reply('You are making a transfer')
+    if (confirmation[m.sender]) return m.reply('Вы совершаете перевод')
     let user = global.db.data.users[m.sender]
     const item = items.filter(v => v in user && typeof user[v] == 'number')
-    let lol = `✳️ Correct use of the command 
-*${usedPrefix + command}*  [item] [quantity] [@user]
+    let lol = `✳️ Правильное использование команды
+*${usedPrefix + command}*  [предмет] [кол-во] [@пользователь]
 
-📌 Example : 
+📌 Пример : 
 *${usedPrefix + command}* exp 65 @${m.sender.split('@')[0]}
 
-📍 transfer items
+📍 Предеты перевода
 ┌──────────────
-▢ *diamond* = Diamonds 💎
-▢ *exp* = Experience 🆙
+▢ *diamond* = Кристалы 💎
+▢ *exp* = Опыт 🆙
 └──────────────
 `.trim()
     const type = (args[0] || '').toLowerCase()
     if (!item.includes(type)) return conn.reply(m.chat, lol, m, { mentions: [m.sender] })
     const count = Math.min(Number.MAX_SAFE_INTEGER, Math.max(1, (isNumber(args[1]) ? parseInt(args[1]) : 1))) * 1
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[2] ? (args[2].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
-    if (!who) return m.reply('✳️ Tag a user')
-    if (!(who in global.db.data.users)) return m.reply(`✳️ User is not in my database`)
-    if (user[type] * 1 < count) return m.reply(`✳️  *${type}*  insufficient to transfer`)
+    if (!who) return m.reply('✳️ Нужно с тегом пользователя')
+    if (!(who in global.db.data.users)) return m.reply(`✳️ Пользователя нет в моей базе`)
+    if (user[type] * 1 < count) return m.reply(`✳️  *${type}* недостаточно для передачи`)
     let confirm = `
-Are you sure you want to transfer *${count}* _*${type}*_ a  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ? 
+Вы уверены что хотите перевести *${count}* _*${type}*_ этому пользователю  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ? 
 
-- Have  *60s* 
-_respond *yes* o *no*_
+- Есть  *60с* 
+_Ответьте *нет* или *да*_
 `.trim()
    
     //conn.sendButton(m.chat, confirm, igfg, null, [['yes'], ['no']], m, { mentions: [who] })
@@ -39,7 +39,7 @@ _respond *yes* o *no*_
         message: m,
         type,
         count,
-        timeout: setTimeout(() => (m.reply('⏳ Time is up'), delete confirmation[m.sender]), 60 * 1000)
+        timeout: setTimeout(() => (m.reply('⏳ Время вышло'), delete confirmation[m.sender]), 60 * 1000)
     }
 }
 
@@ -51,21 +51,21 @@ handler.before = async m => {
     if (m.id === message.id) return
     let user = global.db.data.users[sender]
     let _user = global.db.data.users[to]
-    if (/no?/g.test(m.text.toLowerCase())) {
+    if (/нет?/g.test(m.text.toLowerCase())) {
         clearTimeout(timeout)
         delete confirmation[sender]
-        return m.reply('✅ Transfer cancelled')
+        return m.reply('✅ Перевод отменен')
     }
-    if (/si?/g.test(m.text.toLowerCase())) {
+    if (/да?/g.test(m.text.toLowerCase())) {
         let previous = user[type] * 1
         let _previous = _user[type] * 1
         user[type] -= count * 1
         _user[type] += count * 1
-        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ The transfer was made \n\n*${count}* *${type}*  a @${(to || '').replace(/@s\.whatsapp\.net/g, '')}`, null, { mentions: [to] })
+        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ Перевод выполнен \n\n*${count}* *${type}*  пользователю @${(to || '').replace(/@s\.whatsapp\.net/g, '')}`, null, { mentions: [to] })
         else {
             user[type] = previous
             _user[type] = _previous
-            m.reply(`❎ Error while transferring *${count}* ${type} a *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
+            m.reply(`❎ Ошибка при переводе *${count}* ${type} пользователю *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
         }
         clearTimeout(timeout)
         delete confirmation[sender]
