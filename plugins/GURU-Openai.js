17f3d6c77098da7a37a36d78c3e35816290a4268
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-30 14:30:48 +0600

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 let handler = async (m, { text, usedPrefix, command }) => {
   
   if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response.`;
+    throw `Текст или ответ на сообщение.`;
   }
 
   if (!text && m.quoted && m.quoted.text) {
