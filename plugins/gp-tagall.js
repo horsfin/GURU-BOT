54171f3f10c5f3b2a07871242fff27c4d62e226b
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-30 14:21:27 +0600

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,37 +1,31 @@
-let lastTagallTime = new Map(); // добавляем новую переменную для хранения последнего времени использования команды
+const lastUsed = {}; // Объект для хранения времени последнего использования команды для каждой группы
 
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-    let currentTime = Date.now(); // получаем текущее время
-    let cooldownTime = 24 * 60 * 60 * 1000; // устанавливаем время охлаждения в 24 часа
+    const groupId = m.chat; // Получаем идентификатор группы
+    const currentTime = Date.now();
 
-    let groupId = m.chat; // получаем идентификатор группы
+    // Проверяем, когда была последний раз использована команда в этой группе
+    const lastUsageTime = lastUsed[groupId] || 0;
 
-    if (lastTagallTime.has(groupId)) { // проверяем, использовалась ли команда ранее в этой группе
-        let lastTime = lastTagallTime.get(groupId); // получаем последнее время использования команды
-
-        if (currentTime - lastTime < cooldownTime) { // проверяем, прошло ли уже время охлаждения
-            let remainingTime = cooldownTime - (currentTime - lastTime); // вычисляем оставшееся время охлаждения
-            let remainingHours = Math.floor(remainingTime / (1000 * 60 * 60)); // вычисляем оставшееся количество часов
-            let remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60)); // вычисляем оставшееся количество минут
-
-            m.reply(`Извините, но вы можете использовать эту команду только раз в сутки. Попробуйте еще раз через ${remainingHours} ч. ${remainingMinutes} мин.`); // отправляем сообщение с оставшимся временем
-            return; // прекращаем выполнение команды
-        }
+    // Проверяем, прошло ли уже 24 часа с момента последнего использования
+    if (currentTime - lastUsageTime < 24 * 60 * 60 * 1000) {
+        throw '❌ Эту команду можно использовать раз в сутки.';
     }
 
+    // Если все в порядке, продолжаем выполнение команды
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
-    m.reply(`▢ Группа: *${groupMetadata.subject}*\n▢ Участники: *${participants.length}*${text ? `\n▢ Сообщение: ${text}\n` : ''}\n┌───⊷ *Упоминания*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ RIP ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`▢ Группа : *${groupMetadata.subject}*\n▢ Участники : *${participants.length}*${text ? `\n▢ Сообщение : ${text}\n` : ''}\n┌───⊷ *Упоминание*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ R.I.P ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
     });
 
-    lastTagallTime.set(groupId, currentTime); // обновляем время последнего использования команды
+    // Обновляем время последнего использования команды для этой группы
+    lastUsed[groupId] = currentTime;
 }
 
-handler.help = ['tagall']
-handler.tags = ['group']
-handler.command = ['tagall']
-handler.admin = true
-handler.group = true
+handler.help = ['tagall'];
+handler.tags = ['group'];
+handler.command = ['tagall'];
+handler.admin = true;
+handler.group = true;
 
-export default handler
-
+export default handler;
