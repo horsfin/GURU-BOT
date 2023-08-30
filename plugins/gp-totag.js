660fbe482ca02939e1f923bf541ba8ebc75cd68d
horsfin <141659236+horsfin@users.noreply.github.com>
2023-08-30 14:10:07 +0600

    Update gp-totag.js

--- plugins/gp-totag.js
+++ plugins/gp-totag.js
@@ -1,16 +1,32 @@
+// Создайте объект, чтобы отслеживать время последнего использования команды для каждого пользователя
+const lastUsed = {};
 
-let handler = async (m, { conn, text, participants}) => {
-	
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    if (!m.quoted) throw `✳️ Reply to a message`
-    conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
-}
+let handler = async (m, { conn, text, participants }) => {
+    const userId = m.sender; // Получаем идентификатор отправителя команды
 
-handler.help = ['totag']
-handler.tags = ['group']
-handler.command = /^(totag|tag)$/i
+    // Проверяем, когда была последний раз использована команда для этого пользователя
+    const lastUsageTime = lastUsed[userId] || 0;
+    const currentTime = Date.now();
+    
+    // Проверяем, прошло ли уже 24 часа с момента последнего использования
+    if (currentTime - lastUsageTime < 24 * 60 * 60 * 1000) {
+        throw '❌ Вы можете использовать эту команду только раз в сутки.';
+    }
 
-handler.admin = true
-handler.group = true
+    // Если все в порядке, продолжаем выполнение команды
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+    if (!m.quoted) throw '✳️ Reply to a message';
+    
+    conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users });
 
-export default handler
+    // Обновляем время последнего использования команды для пользователя
+    lastUsed[userId] = currentTime;
+};
+
+handler.help = ['totag'];
+handler.tags = ['group'];
+handler.command = /^(totag|tag)$/i;
+handler.admin = true;
+handler.group = true;
+
+export default handler;
