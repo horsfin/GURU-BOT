a77e42fc86e36cbacc99e7a0e2ba9fe839992819
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-02 16:51:53 +0600

    Update gp-demote.js

--- plugins/gp-demote.js
+++ plugins/gp-demote.js
@@ -26,7 +26,7 @@ m.reply(`✅ Пользователь понижен`)
 }
 handler.help = ['demote (@tag)']
 handler.tags = ['group']
-handler.command = ['demote', 'degradar'] 
+handler.command = ['понизить', 'degradar'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
