0b7367015a787ad5c7f8dc04a95966d7ed0257a8
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 00:47:39 +0600

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,10 +1,10 @@
 import MessageType from '@whiskeysockets/baileys'; 
  const handler = async (m, {conn, usedPrefix, command}) => { 
    const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender)); 
-   if (room == undefined) return conn.sendButton(m.chat, '*[❗] 𝙽𝙾 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*', wm, null, [['𝙸𝙽𝙸𝙲𝙸𝙰𝚁 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾', `${usedPrefix}ttt partida nueva`]], m); 
+   if (room == undefined) return conn.sendButton(m.chat, '*[❗] Ты итак не участвуешь чувак*', wm, null, [['𝙸𝙽𝙸𝙲𝙸𝙰𝚁 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾', `${usedPrefix}ttt partida nueva`]], m); 
    delete conn.game[room.id]; 
-   await m.reply('*[ ✔ ] 𝚂𝙴 𝙴𝙻𝙸𝙼𝙸𝙽𝙾 𝙻𝙰 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾 𝙳𝙴 𝚃𝚁𝙴𝚂 𝙴𝙽 𝚁𝙰𝚈𝙰*'); 
+   await m.reply('*[ ✔ ] Ваша игровая комната была удалена*'); 
  }; 
- handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i; 
+ handler.command = /^(закончитьхо|конец|delxo|deltictactoe)$/i; 
  handler.fail = null; 
- export default handler;
+ export default ;
