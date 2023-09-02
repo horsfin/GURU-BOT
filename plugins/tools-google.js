bc66ff03d7e7a2dcd048ec7b704e6615799642e2
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-02 17:05:20 +0600

    Update tools-google.js

--- plugins/tools-google.js
+++ plugins/tools-google.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, command, args }) => {
 }
 handler.help = ['google']
 handler.tags = ['tools']
-handler.command = ['google'] 
+handler.command = ['гугл'] 
 handler.diamond = true
 
 export default handler
