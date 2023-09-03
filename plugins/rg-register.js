0bb0ff7c1cec1a590344d571c5c56a43f8624c14
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:47:21 +0600

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -5,15 +5,15 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `✳️ Вы уже зарегестрированы\n\nВы хотите перегестрироваться?\n\n 📌 Используй эту команду что-бы удалить вашу регестрацию \n*${usedPrefix}unreg* <Serial number>`
+  if (user.registered === true) throw `✳️ Вы уже зарегестрированы\n\nВы хотите перегестрироваться?\n\n 📌 Используй эту команду что-бы удалить вашу регестрацию \n*${usedPrefix}разрег* <серийный номер>`
   if (!Reg.test(text)) throw `⚠️ Формат не коректен\n\n ✳️ Используй эту команду: *${usedPrefix + command} имя.возраст*\n📌Пример : *${usedPrefix + command}* ${name2}.16`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw '✳️ Имя не может быть пустым'
   if (!age) throw '✳️ Возраст не может быть пустым'
   if (name.length >= 30) throw '✳️Имя слишком длинное' 
   age = parseInt(age)
-  if (age > 100) throw '👴🏻 Ничегосебе дед хочет поиграть с ботом'
-  if (age < 5) throw '🚼 есть дедушка,малыш jsjsj '
+  if (age > 100) throw '👴🏻 Ничего себе дед хочет поиграть с ботом'
+  if (age < 5) throw '🚼 ты ещё мал что-бы со мной общаться'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
@@ -32,7 +32,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
 handler.help = ['reg'].map(v => v + ' <name.age>')
 handler.tags = ['rg']
 
-handler.command = ['verify', 'reg', 'register', 'registrar'] 
+handler.command = ['verify', 'рег', 'register', 'registrar'] 
 
 export default handler
 
