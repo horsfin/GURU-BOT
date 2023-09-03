106ea511bf18851649e7dccc34fc900e7583981e
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:33:35 +0600

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,7 +1,7 @@
 import MessageType from '@whiskeysockets/baileys'; 
  const handler = async (m, {conn, usedPrefix, command}) => { 
    const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender)); 
-   if (room == undefined) return conn.sendButton(m.chat, '*[❗] Ты итак не участвуешь чувак*', wm, null, [['𝙸𝙽𝙸𝙲𝙸𝙰𝚁 𝚂𝙰𝙻𝙰 𝙳𝙴 𝙹𝚄𝙴𝙶𝙾', `${usedPrefix}ttt partida nueva`]], m); 
+   if (room == undefined) return conn.sendButton(m.chat, '*[❗] Ты итак не участвуешь чувак*', wm, null, [['Введи название игровой комнаты', `${usedPrefix}ttt название комнаты`]], m); 
    delete conn.game[room.id]; 
    await m.reply('*[ ✔ ] Ваша игровая комната была удалена*'); 
  }; 
