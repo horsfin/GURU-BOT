6eaa201a3872e86ace25481984acbaad21deed72
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:38:56 +0600

    Update maker.loli.js

--- plugins/maker.loli.js
+++ plugins/maker.loli.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 like you only belong in jail 🚨🚔*', m)
+    }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 Тебе есть место в тюрьме🚨🚔*', m)
     }
     handler.help = ['lolicon']
     handler.tags = ['maker']
-    handler.command = /^(lolicon)$/i
+    handler.command = /^(лоликончик)$/i
     export default handler
