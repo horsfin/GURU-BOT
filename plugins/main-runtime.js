27b2c8656645657bd99cee254eafeda0d4874fe6
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:37:02 +0600

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 handler.help = ['runtime']
 handler.tags = ['main']
-handler.command = ['времярб', 'uptime']
+handler.command = ['времяработы', 'uptime']
 export default handler
 
 function clockString(ms) {
