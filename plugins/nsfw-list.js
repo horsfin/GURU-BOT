bd6da08f78780e8a9a096f941d134f3c406f763c
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:45:12 +0600

    Update nsfw-list.js

--- plugins/nsfw-list.js
+++ plugins/nsfw-list.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, usedPrefix }) => {
-    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
+    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 в группе это запрещено \n\n включите так \n*${usedPrefix}enable* nsfw`
     let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`) 
+    if (user < 17) throw m.reply(`❎ Вам нету 18`) 
    
 m.react(rwait)
     let nsfwCommands = ['xnxx', 'xvid',  'genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts', 'stockings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl'];
   
     let message = `
-  *NSFW Commands Menu* 
+  *NSFW МЕНЮ* 
   
-  Here is the list of available commands for NSFW content. Use the prefix "${usedPrefix}" followed by the command name:
+  Вот список команд. Используйте префикс "${usedPrefix}" а далее название команды:
   
   ${nsfwCommands.map(command => `${usedPrefix}${command}`).join('\n')}
   
-  Note: These commands are only for those who are 18+.
+ Предупреждение:данные команды выдают 18+ картины.
   `;
   
     console.log("Sending message...");
