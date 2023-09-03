ed5d16ed91d579445e07ffe75c3e9dc5b4a6416b
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-03 14:36:06 +0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -7,6 +7,6 @@ function handler(m) {
 
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner'] 
+handler.command = ['владелец', 'creator', 'creador', 'dueño', 'Gowner'] 
 
 export default handler
