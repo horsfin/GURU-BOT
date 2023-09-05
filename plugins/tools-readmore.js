fd033b0cdf8ee47639345de7a489536c0ba56bc3
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-05 19:24:20 +0600

    Update tools-readmore.js

--- plugins/tools-readmore.js
+++ plugins/tools-readmore.js
@@ -2,26 +2,26 @@ let handler = async (m, { conn }) => {
    ayg = global.db.data.users[m.sender] 
   
    if(ayg.pasangan == ""){ 
-     return conn.reply(m.chat,`Anda tidak memiliki pasangan.`,m) 
+     return conn.reply(m.chat,`У тебя нет пары🍪.`,m) 
    } 
   
    beb = global.db.data.users[global.db.data.users[m.sender].pasangan] 
   
    if (typeof beb == "undefined"){ 
-     conn.reply(m.chat,`Berhasil putus hubungan dengan @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: { 
+     conn.reply(m.chat,`Успешно разорвал(а) отношения с @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: { 
        mentionedJid: [global.db.data.users[m.sender].pasangan] 
      }}) 
      ayg.pasangan = "" 
    } 
   
    if (m.sender == beb.pasangan){ 
-     conn.reply(m.chat,`Berhasil putus hubungan dengan @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: { 
+     conn.reply(m.chat,`Успешно разорвал(а) отношения с  @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: { 
        mentionedJid: [global.db.data.users[m.sender].pasangan] 
      }}) 
      ayg.pasangan = "" 
      beb.pasangan = "" 
    }else { 
-     conn.reply(m.chat,`Anda tidak memiliki pasangan.`,m) 
+     conn.reply(m.chat,`У тебя нет пары.`,m) 
    } 
  } 
  handler.help = ['putus'] 
