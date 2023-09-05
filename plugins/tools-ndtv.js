122c74586f7c32546ef2300da22ae3c820362049
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-05 19:51:55 +0600

    Update tools-ndtv.js

--- plugins/tools-ndtv.js
+++ plugins/tools-ndtv.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => { 
-   if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `Kamu sedang tidak menembak siapapun!`, m) 
-   if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `Kamu telah berpacaran dengan @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: { 
+   if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `Ты никому не предлагал!`, m) 
+   if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `Ты встречаешься с @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: { 
      mentionedJid: [global.db.data.users[m.sender].pasangan] 
    }}) 
-   conn.reply(m.chat, `Kamu sudah mengikhlaskan @${global.db.data.users[m.sender].pasangan.split('@')[0]} karena dia tidak memberikan jawaban diterima atau ditolak`, m, {contextInfo: { 
+   conn.reply(m.chat, `Ты был отвержен @${global.db.data.users[m.sender].pasangan.split('@')[0]} потому что он не дал ответа, принятия или отвержения`, m, {contextInfo: { 
      mentionedJid: [global.db.data.users[m.sender].pasangan] 
    }}) 
    global.db.data.users[m.sender].pasangan = "" 
