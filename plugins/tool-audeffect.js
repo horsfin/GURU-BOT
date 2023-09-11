ef9ed528d5ba5ce0a2caf1c445882a791f15aa2c
horsfin <141659236+horsfin@users.noreply.github.com>
2023-09-11 13:33:52 +0600

    Update tool-audeffect.js

--- plugins/tool-audeffect.js
+++ plugins/tool-audeffect.js
@@ -31,7 +31,7 @@ conn.sendFile(m.chat, buff, ran, null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })})
-} else throw `*Reply your audio or vn that will be modified, using the command ${usedPrefix + command}*`
+} else throw `*Ответьте на ваш аудио- или видеофайл, который будет изменен, используя команду ${usedPrefix + command}*`
 } catch (e) {
 throw e
 }}
